fd221bc0b2ee322c69d8cf4bd6084b5a5d54fe7c
hasmira26 <hhasmira323@gmail.com>
2026-09-02 22:18:55 +0700

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,8 @@
+public class HelloWord { //Deklarasi class (nama kelas)
+
+    public static void main(String[] args) { //method utama (titik masuk entry poin) 
+       System.out.println("Hello Word"); // Untuk menampilkan output
+       //itik koma di menandakan intruksi java diakhiri
+       //kurung kurawal merupakan blok kode awal dan akhir(class/method) 
+    }
+}
