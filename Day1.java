0b153a9dac2d7da12357e8162faa95357c8f5923
hasmira26 <hhasmira323@gmail.com>
2026-09-02 23:57:00 +0800

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,8 +1,18 @@
-public class HelloWord { //Deklarasi class (nama kelas)
 
-    public static void main(String[] args) { //method utama (titik masuk entry poin) 
-       System.out.println("Hello Word"); // Untuk menampilkan output
-       //itik koma di menandakan intruksi java diakhiri
-       //kurung kurawal merupakan blok kode awal dan akhir(class/method) 
+/**
+ * selamat malam semuanya.
+ * @author hasmira
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        /*
+         * Bagian ini mendeklarasikan
+         * titikk utama masuk entry poin
+         */
+       
+
+        // Menampilkan output ke layar
+        System.out.println("Halo saya mira");
     }
 }
