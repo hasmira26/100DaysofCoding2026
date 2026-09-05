a3f3aac754ca0867eb70643add348d924be2dfc4
hasmira26 <hhasmira323@gmail.com>
2026-09-05 12:05:49 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+public class TipeDataNumerik {
+    public static void main(String[] args) {
+        
+        // byte - untuk angka kecil (-128 sampai 127)
+        byte umur = 20;
+        System.out.println("Umur: " + umur);
+        
+        // short - untuk angka menengah (-32768 sampai 32767)
+        short tahun = 2026;
+        System.out.println("Tahun: " + tahun);
+        
+        // int - untuk angka sehari-hari (paling sering dipakai)
+        int jumlahBarang = 150;
+        System.out.println("Jumlah barang: " + jumlahBarang);
+        
+        // long - untuk angka sangat besar (harus pakai huruf L di belakang)
+        long jumlahPenduduk = 8000000000L;
+        System.out.println("Jumlah penduduk: " + jumlahPenduduk);
+        
+    }
+}
