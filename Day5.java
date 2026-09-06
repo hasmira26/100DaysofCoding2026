913d8b7efb57c2c241645307ddf1929b5acb1fff
hasmira26 <hhasmira323@gmail.com>
2026-09-06 12:55:06 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,10 @@
+public class TypeDataPecahan {
+    public static void main(String[] args) {
+    
+        float f = 3.14f;
+        double d = 3.14159265358979;
+
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+}
