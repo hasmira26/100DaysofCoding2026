ed6a5c9d197e294b0f58bb5145515ca261d3d2dd
hasmira26 <hhasmira323@gmail.com>
2026-09-07 15:46:20 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,22 @@
+public class BooleanCart {
+
+    public static void main(String[] args) {
+        
+        char huruf = 'A';
+        char angka = '7';
+        char simbol = '$';
+
+        boolean lulus = true;
+        boolean gagal = false;
+
+        System.out.println(huruf);
+        System.out.println(angka);
+        System.out.println(simbol);
+
+         if (lulus) {
+            System.out.println("Selamat, kamu lulus!");
+            } else {
+            System.out.println("Coba lagi ya.");
+            }
+    }
+}
