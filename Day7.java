0bb1f26351ce68e9fba912b8a965c2f38ff64e61
hasmira26 <hhasmira323@gmail.com>
2026-09-08 14:49:49 +0700

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,6 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String teks = "Halo semua nama saya hasmira saya sedang belajar di parang parang bersama teman teman";
+        System.out.println(teks);
+    }
+}
