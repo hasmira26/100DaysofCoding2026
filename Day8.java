9cf19acf2774a4d0b3a9363a5a9ce5738bfe0c2d
hasmira26 <hhasmira323@gmail.com>
2026-09-09 15:09:23 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+public class DeklarasiVariabel {
+
+    public static void main(String[] args) {
+      //Deklarasi & inisial//
+        String nama = "Hasmira";
+        int umur = 18 + 1;
+        double tb = 150.0;
+        char Jk = 'p';
+        
+        System.out.println("====BIODATA MAHASISWA====");
+        System.out.println("Nama    :" + nama);
+        System.out.println("Umur    :" + umur);
+        System.out.println("Tinggi  :" + tb);
+        System.out.println("Gendre  :" + Jk);
+
+    }
+}
