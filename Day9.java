fea613c35cc5b8839feed258c37689430564bdde
hasmira26 <hhasmira323@gmail.com>
2026-09-10 14:43:45 +0700

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,16 @@
+/**
+ * konstanta
+ */
+public class konstanta {
+
+    public static void main(String[] args) {
+        int nilaiBiasa = 10;
+        
+        final int bulan = 12;
+
+        System.out.println("Nilai biasa: "  + nilaiBiasa);
+        System.out.println("Nilai yang tdk dapat di ubah: "  + bulan);
+
+
+    }
+}
