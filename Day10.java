ff403381001b230e32198e2d1e5d2186d8b48ca8
hasmira26 <hhasmira323@gmail.com>
2026-09-11 14:47:39 +0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,46 @@
+ /**
+  * Penggunaan MIN/MAX
+  * BatasMinMaks
+  */
+
+
+public class BatasMinMaks {
+    public static void main(String[] args) {
+        // byte (8 bit)
+        System.out.println("=== byte ===");
+        System.out.println("Min: " + Byte.MIN_VALUE);
+        System.out.println("Max: " + Byte.MAX_VALUE);
+
+        // short (16 bit)
+        System.out.println("\n=== short ===");
+        System.out.println("Min: " + Short.MIN_VALUE);
+        System.out.println("Max: " + Short.MAX_VALUE);
+
+        // int (32 bit)
+        System.out.println("\n=== int ===");
+        System.out.println("Min: " + Integer.MIN_VALUE);
+        System.out.println("Max: " + Integer.MAX_VALUE);
+
+        // long (64 bit)
+        System.out.println("\n=== long ===");
+        System.out.println("Min: " + Long.MIN_VALUE);
+        System.out.println("Max: " + Long.MAX_VALUE);
+
+        // float
+        System.out.println("\n=== float ===");
+        System.out.println("Min: " + Float.MIN_VALUE);
+        System.out.println("Max: " + Float.MAX_VALUE);
+
+        // double(Double)
+        System.out.println("\n=== double ===");
+        System.out.println("Min: " + Double.MIN_VALUE);
+        System.out.println("Max: " + Double.MAX_VALUE);
+
+        // char (Character)
+        System.out.println("\n=== char ===");
+        System.out.println("Min: " + (int) Character.MIN_VALUE);
+        System.out.println("Max: " + (int) Character.MAX_VALUE);
+
+
+    }
+}
