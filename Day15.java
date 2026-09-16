7fbc4c282368cdbb106d80d0530258d3e5b08a5c
hasmira26 <hhasmira323@gmail.com>
2026-09-16 14:39:16 +0700

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,14 @@
+/**
+ * Day15
+ */
+public class Day15 {
+
+    public static void main(String[] args) {
+        // operasi Aritmaatika
+        int angka1 = 6;
+        int angka2 = 3;
+
+        System.out.println(angka1*angka2);
+        System.out.println(angka1/angka2);
+    }
+}
