562d7248581a3c20cec869cd9bcb9faf65f738a3
hasmira26 <hhasmira323@gmail.com>
2026-09-17 12:52:26 +0700

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,12 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 5;
+        
+        int hasilBagi = a / b;
+        int sisaBagi = a % b;
+        
+        System.out.println("Hasil bagi: " + hasilBagi);
+        System.out.println("Sisa bagi: " + sisaBagi);
+    }
+}
